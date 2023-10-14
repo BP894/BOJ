115d0ac994b7ef2bbee4041194b723648c801a43
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 20:13:05 +0900

    11021. A + B - 7

---
+++ Loop/11021/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace _11021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder answer = new StringBuilder();
+
+            int num = Int32.Parse(Console.ReadLine()!);
+
+            int[] sum = new int[num];
+            for (int i = 0; i < num; i++)
+            {
+                string[] inputs = Console.ReadLine()!.Split();
+                int[] numbers = Array.ConvertAll(inputs, Int32.Parse);
+
+                sum[i] = numbers[0] + numbers[1];
+
+                answer.AppendLine($"Case #{i + 1}: " + sum[i].ToString());
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
