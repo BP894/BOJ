1d2792b94cc2d91ac67aba6ea4db04fdbff6241f
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 19:01:19 +0900

    14681. 사분면 고르기

---
+++ Conditional Statement/14681/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _14681
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+
+            if (a > 0)
+            {
+                if (b > 0)
+                    Console.Write("1");
+                if (b < 0)
+                    Console.Write("4");
+            }
+
+            if (a < 0)
+            {
+                if (b > 0)
+                    Console.Write("2");
+                if (b < 0)
+                    Console.Write("3");
+            }
+        }
+    }
+}
