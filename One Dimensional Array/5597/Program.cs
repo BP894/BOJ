e2510d45e31d81f8beaad9eb21bfa19b75256da7
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 01:40:02 +0900

    5597. 과제 안내신 분...?

---
+++ One Dimensional Array/5597/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _5597
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] students = new int[31];
+            for (int i = 0; i < 28; i++)
+            {
+                int student = Int32.Parse(Console.ReadLine()!);
+                students[student] = 1;
+            }
+            for (int i = 1; i < students.Length; i++)
+            {
+                if (students[i] != 1)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
