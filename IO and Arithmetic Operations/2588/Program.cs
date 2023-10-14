0ba347d4e6c8758cc63d36dc06a499d53f5b73a7
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 18:50:30 +0900

    2588. 곱셈

---
+++ IO and Arithmetic Operations/2588/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _2588
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+
+            int c = a * (b % 10);
+            int d = a * ((b / 10) % 10);
+            int e = a * ((b / 10) / 10);
+
+            Console.WriteLine("{0}", c);
+            Console.WriteLine("{0}", d);
+            Console.WriteLine("{0}", e);
+            Console.WriteLine("{0}", c + d * 10 + e * 100);
+        }
+    }
+}
