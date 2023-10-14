a8f39a520c32cfdc1f2b4b21df7c852d1d8340e2
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 18:45:56 +0900

    10926.??!

---
+++ IO and Arithmetic Operations/10926/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _10926
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.Write(input + "??!");
+        }
+    }
+}
