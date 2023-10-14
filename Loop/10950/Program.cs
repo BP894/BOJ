aa7016868f8f15bfb726e3d70e4038fcdb70fac5
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 20:06:47 +0900

    10950. A + B - 3

---
+++ Loop/10950/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _10950
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Int32.Parse(Console.ReadLine()!);
+            int[] results = new int[T];
+            
+            for(int i = 0; i < T; i++)
+            {
+                string[] inputs = Console.ReadLine()!.Split(' ');
+                int A = int.Parse(inputs[0]);
+                int B = int.Parse(inputs[1]);
+
+                results[i] = A + B;
+            }
+
+            for(int i = 0; i < T; i++)
+                Console.WriteLine(results[i]);
+        }
+    }
+}
