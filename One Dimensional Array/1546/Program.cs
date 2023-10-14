61cd95f3d552adc32f2b42966d6570d3ce433b3f
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 02:17:34 +0900

    1546. 평균

---
+++ One Dimensional Array/1546/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _1546
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+            int[] scores = Array.ConvertAll(Console.ReadLine()!.Split(' '), i => int.Parse(i));
+            Array.Sort<int>(scores);
+            double M = scores[N - 1];
+
+            double sum = 0;
+            for(int i = 0; i < scores.Length; i++) 
+            {
+                double score = Math.Round(scores[i] / M * 100, 2);
+                sum += score;
+            }
+
+            Console.WriteLine(sum / N);
+        }
+    }
+}
