b9a638a8e9ec589d6eae67e0e2bab119034df21d
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 18:41:15 +0900

    10998.A x B

---
+++ IO and Arithmetic Operations/10998.AxB/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _10998.AxB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = Int32.Parse(input[0]);
+            int b = Int32.Parse(input[1]);
+
+            Console.Write("{0}", a * b);
+        }
+    }
+}
