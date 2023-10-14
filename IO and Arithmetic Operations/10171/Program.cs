4bd91bbfc396fcc4629a8920bec8afb69676ec54
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 18:52:45 +0900

    10171. 고양이

---
+++ IO and Arithmetic Operations/10171/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _10171
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
