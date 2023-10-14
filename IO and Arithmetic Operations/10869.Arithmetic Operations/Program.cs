c39dd3cba52dba29bb14a91869b86013b07dc42a
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 18:44:47 +0900

    10869. Arithmetic Operations

---
+++ IO and Arithmetic Operations/10869.Arithmetic Operations/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _10869.Arithmetic_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = Int32.Parse(input[0]);
+            int b = Int32.Parse(input[1]);
+
+            Console.Write("{0}\n{1}\n{2}\n{3}\n{4}", a + b, a - b, a * b, a / b, a % b);
+        }
+    }
+}
