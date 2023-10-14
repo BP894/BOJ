b1fadc33a46e05f699692161ea1a8e84a782bd20
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 01:19:38 +0900

    10818. 최소, 최대

---
+++ One Dimensional Array/10818/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace _10818
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine()!);
+            string[] inputs = Console.ReadLine()!.Split(' ');
+            int min = int.MaxValue; int max = int.MinValue;
+
+            foreach (string input in inputs)
+            {
+                if(int.Parse(input) < min)
+                {
+                    min = int.Parse(input);
+                }
+                if(int.Parse(input) > max)
+                {
+                    max = int.Parse(input);
+                }
+            }
+            Console.WriteLine($"{min} {max}");
+        }
+        
+    }
+}
