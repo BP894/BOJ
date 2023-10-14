0e8cc2a749259e6c67e95044e6cd43a7c50c09d1
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 18:58:30 +0900

    9498. 시험 성적

---
+++ Conditional Statement/9498/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _9498
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int score = Int32.Parse(input);
+
+            if (score >= 90 && score < 101)
+                Console.Write("A");
+            if (score >= 80 && score < 90)
+                Console.Write("B");
+            if (score >= 70 && score < 80)
+                Console.Write("C");
+            if (score >= 60 && score < 70)
+                Console.Write("D");
+            if (score < 60)
+                Console.Write("F");
+        }
+    }
+}
