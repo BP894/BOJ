811ca5a8b7d8b5dca59c250a649ad5ebb21277b0
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 18:47:45 +0900

    18108. 1998년생인 내가 태국에서는 2541년생?!

---
+++ IO and Arithmetic Operations/18108/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _18108
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int year1 = Int32.Parse(input);
+            int year2 = year1 - 2541 + 1998;
+            Console.Write("{0}", year2);
+        }
+    }
+}
