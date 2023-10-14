227a949953bae875b67b9ff4897887df4feef2fc
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 01:25:07 +0900

    2562. 최댓값

---
+++ One Dimensional Array/2562/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _2562
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MinValue;
+            int index = 0;
+
+            for(int i = 1; i <= 9; i++)
+            {
+                int num = int.Parse(Console.ReadLine()!);
+                if(num > max)
+                {
+                    max = num;
+                    index = i;
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(index);
+        }
+    }
+}
