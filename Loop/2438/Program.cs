d4060ed1670dbed7e316cd054aee3912ad467bef
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 20:21:01 +0900

    2438. 별 찍기 1

---
+++ Loop/2438/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _2438
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+
+            for(int i = 0; i < N; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
