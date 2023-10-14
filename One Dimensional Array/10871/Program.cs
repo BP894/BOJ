bd6f9e70bc4ec0a34a1ad23464b4b143c45036a0
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 21:20:18 +0900

    10871. X보다 작은 수

---
+++ One Dimensional Array/10871/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace _10871
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine()!.Split(' ');
+            int N = int.Parse(inputs[0]);
+            int X = int.Parse(inputs[1]);
+
+            string[] A = Console.ReadLine()!.Split(' ');
+
+            StringBuilder answer = new StringBuilder();
+            foreach(string a in A)
+            {
+                if(int.Parse(a) < X)
+                    answer.Append(a + " ");
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
