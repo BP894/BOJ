1820af16e07cb6dcffc35d1ab680e1df74d895c0
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 18:48:39 +0900

    10430. 나머지

---
+++ IO and Arithmetic Operations/10430/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _10430
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = Int32.Parse(input[0]);
+            int b = Int32.Parse(input[1]);
+            int c = Int32.Parse(input[2]);
+
+            Console.WriteLine("{0}", (a + b) % c);
+            Console.WriteLine("{0}", ((a % c) + (b % c)) % c);
+            Console.WriteLine("{0}", (a * b) % c);
+            Console.WriteLine("{0}", ((a % c) * (b % c)) % c);
+        }
+    }
+}
