27be42af07f7299e170e1717b3396d40b93428a4
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 01:38:00 +0900

    10810. 공 넣기

---
+++ One Dimensional Array/10810/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _10810
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] NM = Console.ReadLine()!.Split(' ');
+            int N = int.Parse(NM[0]);
+            int M = int.Parse(NM[1]);
+
+            int[] baskets = new int[N];
+
+            for(int a = 0; a < M; a++)
+            {
+                string[] ijk = Console.ReadLine()!.Split(' ');
+                int i = int.Parse(ijk[0]);
+                int j = int.Parse(ijk[1]);
+                int k = int.Parse(ijk[2]);
+
+                for(int b = i; b <= j; b++)
+                {
+                    baskets[b - 1] = k; 
+                }
+            }
+            
+            foreach(int ball in baskets) 
+            {
+                Console.Write(ball + " ");
+            }
+        }
+    }
+}
