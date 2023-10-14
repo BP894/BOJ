f607800176dde4cb974a874d9c58c402d742f381
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 19:56:10 +0900

    2739. 구구단

---
+++ Loop/2739/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _2739
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine()!);
+            int i = 0;
+
+            while(true)
+            {
+                if (++i >= 10)
+                    break;
+
+                Console.WriteLine($"{a} * {i} = {a * i}");
+            }
+        }
+    }
+}
