160c4e2aa106ee023bb058ac2f56d307246097f5
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 20:07:54 +0900

    8393. 합

---
+++ Loop/8393/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _8393
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine()!;
+            int num = Int32.Parse(input);
+            int sum = 0;
+
+            while (num > 0)
+            {
+                sum += num;
+                num--;
+            }
+
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
