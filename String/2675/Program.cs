c56a6adbd98d712a7d11b3c4d5be0656024323d5
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 03:25:47 +0900

    2675. 문자열 반복

---
+++ String/2675/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace _2675
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine()!);
+            int R = 0;
+            string S = "";
+            StringBuilder answer = new StringBuilder();
+
+            for (int i = 0; i < T; i++)
+            {
+                string[] RS = Console.ReadLine()!.Split(' ');
+                R = int.Parse(RS[0]);
+                S = RS[1];
+
+                foreach (char s in S)
+                {
+                    for (int j = 0; j < R; j++)
+                    {
+                        answer.Append(s);
+                    }
+                }
+                answer.AppendLine();
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
