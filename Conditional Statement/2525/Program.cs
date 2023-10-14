04db2d28235c10763108a0a3e5a8231785992aba
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 19:32:59 +0900

    2525. 오븐 시계

---
+++ Conditional Statement/2525/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _2525
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()!.Split(' ');
+            int h = Int32.Parse(input[0]);
+            int m = Int32.Parse(input[1]);
+
+            int ct = Int32.Parse(Console.ReadLine()!);
+
+            m = m + ct;
+
+            if(m >= 60)
+            {
+                h = h + m / 60;
+                m = m % 60;
+            }
+
+            if(h >= 24)
+            {
+                h -= 24;
+            }
+
+            Console.Write("{0} {1}", h, m);
+        }
+    }
+}
