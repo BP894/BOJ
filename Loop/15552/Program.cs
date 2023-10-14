ef9876609772725efcebead0211601ad7b6458a2
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 20:12:05 +0900

    15552. 빠른 A + B

---
+++ Loop/15552/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace _15552
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder answer = new StringBuilder();
+
+            int num = Int32.Parse(Console.ReadLine()!);
+
+            int[] sum = new int[num];
+            for (int i = 0; i < num; i++)
+            {
+                string[] inputs = Console.ReadLine()!.Split();
+                int[] numbers = Array.ConvertAll(inputs, Int32.Parse);
+
+                sum[i] = numbers[0] + numbers[1];
+
+                answer.AppendLine(sum[i].ToString());
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
