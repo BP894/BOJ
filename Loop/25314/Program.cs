3b743e1c0ccd81b79557e182f34069bd12e346fd
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 20:09:55 +0900

    25314. 코딩은 체육과목 입니다.

---
+++ Loop/25314/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _25314
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine()!;
+            int num = Int32.Parse(input);
+
+            num /= 4;
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write("long ");
+            }
+            Console.Write("int");
+        }
+    }
+}
