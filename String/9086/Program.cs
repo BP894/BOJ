b042ed3a0adacf74683e5e0aaed70a676275e4e5
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 02:35:30 +0900

    9086. 문자열

---
+++ String/9086/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace _9086
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine()!);
+
+            StringBuilder answer = new StringBuilder();
+            for(int i = 0; i < T; i++)
+            {
+                string t = Console.ReadLine()!;
+                answer.Append(t.First());
+                answer.Append(t.Last());
+                answer.AppendLine();
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
