39331285946b9a4345910a557ae98b20b3759bb9
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 18:59:30 +0900

    2753. 윤년

---
+++ Conditional Statement/2753/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _2753
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Int32.Parse(Console.ReadLine());
+
+            if (input % 4 == 0)
+            {
+                if (input % 100 != 0 || input % 400 == 0)
+                    Console.Write("1");
+                else
+                    Console.Write("0");
+            }
+            else
+                Console.Write("0");
+        }
+    }
+}
