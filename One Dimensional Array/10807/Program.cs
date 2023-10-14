20ca082d62a2044c1e75d92f725ba4ef78e6c817
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 20:57:25 +0900

    10807. 개수 세기

---
+++ One Dimensional Array/10807/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _10807
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input_1 = Console.ReadLine()!;
+            int num_1 = Int32.Parse(input_1);
+
+            string[] input_2 = Console.ReadLine()!.Split(' ');
+            int[] num_2 = Array.ConvertAll(input_2, Int32.Parse);
+
+            string input_3 = Console.ReadLine()!;
+            int num_3 = Int32.Parse(input_3);
+
+            int result = 0;
+            foreach (int i in num_2)
+                if (i == num_3)
+                    result++;
+
+            Console.WriteLine(result);
+        }
+    }
+}
