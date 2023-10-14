0b78677ee7c288ff0fdb3f7cded90df18dffc1eb
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 20:33:41 +0900

    2439. 별 찍기 2

---
+++ Loop/2439/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _2439
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+            for(int i = 1; i <= N; i++)
+            {
+                for(int j = 1; j <= N; j++)
+                {
+                    if (j < N - i + 1)
+                        Console.Write(" ");
+                    else
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Loop/별 찍기 2/Program.cs
@@ -0,0 +1,10 @@
+﻿amespace 별_찍기_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
