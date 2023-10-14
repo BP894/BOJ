4aa29a89a0fcdae3f5bbea892275d20b049e210e
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 18:43:02 +0900

    1008.AB

---
+++ IO and Arithmetic Operations/1008.AB/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _1008.AB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            double a = Double.Parse(input[0]);
+            double b = Double.Parse(input[1]);
+
+            Console.Write("{0}", a / b);
+        }
+    }
+}
