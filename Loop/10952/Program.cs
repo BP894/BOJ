8812cd3f9e57d6bf5dc0e3e4df85dd9a18939ea7
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 20:40:09 +0900

    10952. A + B - 5

---
+++ Loop/10952/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace _10952
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder answer = new StringBuilder();
+
+            while (true)
+            {
+                string[] inputs = Console.ReadLine()!.Split(' ');
+                int A = int.Parse(inputs[0]);
+                int B = int.Parse(inputs[1]);
+
+                if (A == 0 && B == 0)
+                    break;
+
+                answer.AppendLine((A + B).ToString());
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
