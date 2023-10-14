cc8327d4881975cd874408f914854dc7dd3d111d
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-14 19:25:07 +0900

    2884. 알람 시계

---
+++ Conditional Statement/2884/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _2884
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Input = Console.ReadLine()!.Split(' ');
+            int h = Int32.Parse(Input[0]);
+            int m = Int32.Parse(Input[1]);
+
+            m -= 45;
+
+            if (m < 0)
+            {
+                m = m + 60;
+                h--;
+            }
+
+            if (h < 0)
+            {
+                h = h + 24;
+            }
+
+            Console.WriteLine($"{h} {m}");
+        }
+    }
+}
