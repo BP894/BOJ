07b54ccf6bc7afa6c3fbd05c8ec509cb6176e33b
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 01:49:02 +0900

    3052. 나머지

---
+++ One Dimensional Array/3052/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace _3052
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] isExist = new bool[42];
+            int result = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                int num = int.Parse(Console.ReadLine()!);
+                num %= 42;
+                if (isExist[num] == true)
+                    continue;
+                result++;
+                isExist[num] = true;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
