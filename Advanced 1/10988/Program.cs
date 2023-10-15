b5641644ed658bf0950d805f5ff576bc4072f42b
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 23:33:15 +0900

    10988. 팰린드롬인지 확인하기

---
+++ Advanced 1/10988/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _10988
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine()!;
+            int split_standard = input.Length / 2;
+
+            string string_01 = input.Substring(0, split_standard);
+            string string_02 = input.Substring(input.Length - split_standard);
+
+            string_02 = new string(string_02.Reverse().ToArray());
+
+            if (string_01.Equals(string_02))
+                Console.WriteLine("1");
+            else
+                Console.WriteLine("0");
+        }
+    }
+}
