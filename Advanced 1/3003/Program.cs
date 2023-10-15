da9efc9bd55854696d97ccb78b2cd67e2dd2b9a3
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 23:15:16 +0900

    3003. 킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ Advanced 1/3003/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _3003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int KING = 1;
+            const int QUEEN = 1;
+            const int ROOK = 2;
+            const int BISHOP = 2;
+            const int KNIGHT = 2;
+            const int PAWN = 8;
+
+            string[] input = Console.ReadLine()!.Split(' ');
+            int[] pieces = Array.ConvertAll(input, item => int.Parse(item));
+
+            ToRightPieceNumber(ref pieces[0], KING);
+            ToRightPieceNumber(ref pieces[1], QUEEN);
+            ToRightPieceNumber(ref pieces[2], ROOK);
+            ToRightPieceNumber(ref pieces[3], BISHOP);
+            ToRightPieceNumber(ref pieces[4], KNIGHT);
+            ToRightPieceNumber(ref pieces[5], PAWN);
+
+            foreach (int piece in pieces)
+            {
+                Console.Write(piece + " ");
+            }
+            
+        }
+        static void ToRightPieceNumber(ref int input, int piece)
+        {
+            input = piece - input;
+        }
+    }
+}
