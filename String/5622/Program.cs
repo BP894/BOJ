a6bfc3438167582d889b842028cc029ce807f679
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 22:15:53 +0900

    5622. 다이얼

---
+++ String/5622/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _5622
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine()!;
+            int sum = 0;
+            foreach (char i in input)
+            {
+                sum += (i) switch
+                {
+                    <= 'C' => 2 + 1,
+                    <= 'F' => 3 + 1,
+                    <= 'I' => 4 + 1,
+                    <= 'L' => 5 + 1,
+                    <= 'O' => 6 + 1,
+                    <= 'S' => 7 + 1,
+                    <= 'V' => 8 + 1,
+                    <= 'Z' => 9 + 1,
+                    _ => 0 + 10
+                };
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
