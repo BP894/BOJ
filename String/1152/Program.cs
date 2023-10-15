e4d1000fa9865770c6b74b64bf8f13b21d494795
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 21:19:40 +0900

    1152. 단어의 개수

---
+++ String/1152/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace _1152
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine()!.Trim();
+            string[] words = input.Split(' ');
+            int answer = words.Length;
+            if (words[0].Equals(""))
+                answer--;
+            Console.WriteLine(answer);
+        }
+    }
+}
