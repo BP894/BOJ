b2ed35b5c784ce8baac84d14fbf340363733e348
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-16 00:32:37 +0900

    1157. 단어 공부

---
+++ Advanced 1/1157/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Headers;
+
+namespace _1157
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] alpabet = new int[26];
+
+            string input = Console.ReadLine()!.ToUpper();
+
+            int max = int.MinValue;
+            char max_ch = ' ';
+
+            foreach (char ch in input)
+                alpabet[ch - 'A']++;
+            foreach (int v in alpabet)
+                Console.Write(v + " ");
+
+            for (int i = 0; i < alpabet.Length; i++)
+            {
+                if (alpabet[i] > max )
+                {
+                    max = alpabet[i];
+                    max_ch = (char)(i + 'A');
+                }
+                else if (max == alpabet[i])
+                    max_ch = '?';
+            }
+            Console.WriteLine(max_ch);
+        }
+    }
+}
