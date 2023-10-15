b68afa68ab7394d3a6b5e5782b6793ee49caaca5
BP894 <90163997+BP894@users.noreply.github.com>
2023-10-15 21:30:36 +0900

    2908. 상수

---
+++ String/2908/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _2908
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()!.Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+
+            ReverseNumber(ref A);
+            ReverseNumber(ref B);
+
+            Console.WriteLine(A > B ? A : B);
+        }
+        static void ReverseNumber(ref int num)
+        {
+            num = num / 100 + ((num % 100) / 10) * 10 + (num % 10) * 100;
+        }
+    }
+}
